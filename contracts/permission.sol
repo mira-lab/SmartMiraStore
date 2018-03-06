cca44ca1749d10b0745172a346b3a5e1a57f51f9
Andrey Tarasenko <andrey.tarasenko@finsight.com>
2018-03-06 13:02:37 +0200

    smart contract using struct

--- contracts/permission.sol
+++ contracts/permission.sol
@@ -1,69 +1,71 @@
 pragma solidity ^0.4.11;
 
-contract SecretStorePermissions {
-    mapping (uint => bytes32) keysIndex;
-    mapping (bytes32 => address) keyOwner;
-    mapping (bytes32 => bytes32) pins;
+contract PinSecretStorePermissions {
+    address private _masterAdmin;
+
+    struct SecretStoreKey {
+        address keyOwner;
+        bytes32 pin;
+        bool allowChangeOwner;
+    }
+
+    mapping (bytes32 => SecretStoreKey) secretStoreKeys;
     uint keysCount;
 
-    function addKeyAccess(address user, bytes32 document, bytes32 pin) public returns (bool) {
-        for(uint i = 0; i < keysCount; i++) {
-            if (keysIndex[i] == document) {
-                return false;
-            }
+    function PinSecretStorePermissions() {
+        _masterAdmin = msg.sender;
+    }
+
+    function changePin(bytes32 document, bytes32 oldPin, bytes32 newPin, bool allowChangeOwner) public returns (bool) {
+        require(secretStoreKeys[document]);
+
+        if ((msg.sender != secretStoreKeys[document].keyOwner) || (secretStoreKeys[document].pin != oldPin)) {
+            return false;
         }
-        keysIndex[keysCount] = document;
-        keyOwner[document] = user;
-        pins[document] = pin;
+        secretStoreKeys[document].pin = newPin;
+        secretStoreKeys[document].allowChangeOwner = allowChangeOwner;
+        return true;
+    }
+
+    function changeOwner(bytes32 document, bytes32 accessPin, bytes32 setPin) public returns (bool) {
+        require(secretStoreKeys[document]);
+
+        if ((!secretStoreKeys[document].allowChangeOwner) || (secretStoreKeys[document].pin != accessPin)) {
+            return false;
+        }
+        secretStoreKeys[document].pin = setPin;
+        secretStoreKeys[document].allowChangeOwner = false;
+        return true;
+    }
+
+    function addKey(bytes32 document, address user, bytes32 pin) public returns (bool) {
+        require(!secretStoreKeys[document]);
+
+        secretStoreKeys[document] = {
+            keyOwner: user,
+            pin: pin,
+            allowChangeOwner: false
+        }
         keysCount = keysCount + 1;
         return true;
     }
 
-    function rmKeyAccess(address user, bytes32 document, bytes32 pin) public returns (bool) {
-        for(uint i = 0; i < keysCount; i++) {
-            if (keysIndex[i] == document) {
-                if ((keyOwner[document] == user) && (pins[document] == pin)) {
-                    delete keysIndex[i];
-                    delete keyOwner[document];
-                    delete pins[document];
-                    keysCount = keysCount - 1;
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-        return false;
-    }
-
-    function checkKeyAccess(address user, bytes32 document, bytes32 pin) public returns (bool) {
-        for(uint i = 0; i < keysCount; i++) {
-            if (keysIndex[i] == document) {
-                return (keyOwner[document] == user) && (pins[document] == pin);
-            }
-        }
-        return false;
+    function checkKeyAccess(bytes32 document, address user, bytes32 pin) public returns (bool) {
+        require(secretStoreKeys[document]);
+        return (secretStoreKeys[i].keyOwner == user) && (secretStoreKeys[i].pin == pin);
     }
 
     function whoAccess(bytes32 document) public returns (address) {
-        for(uint i = 0; i < keysCount; i++) {
-            if (keysIndex[i] == document) {
-                return keyOwner[document];
-            }
-        }
-        return 0;
+        require(secretStoreKeys[document]);
+        return secretStoreKeys[document].keyOwner;
     }
 
     function permittedCount(bytes32 document) public returns (uint256) {
         return keysCount;
     }
 
-    function checkPermissions(address user, bytes32 document) public constant returns (bool) {
-        for(uint i = 0; i < keysCount; i++) {
-            if ((keysIndex[i] == document) && (keyOwner[keysIndex[i]] == user)) {
-                return true;
-            }
-        }
-        return false;
+    function checkPermissions(bytes32 document, address user) public constant returns (bool) {
+        require(secretStoreKeys[document]);
+        return secretStoreKeys[document].keyOwner == user;
     }
 }
